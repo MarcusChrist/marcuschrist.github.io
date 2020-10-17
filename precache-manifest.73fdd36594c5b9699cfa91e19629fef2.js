aa29113c76ce5710883854b3b3a0e29f2d46ef7e
Marcus Christiansson (Ext) <ext.marcus.christiansson@tieto.com>
2020-10-18 01:08:29 +0200

    Updates

---
+++ precache-manifest.73fdd36594c5b9699cfa91e19629fef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07821ac76e8a28a48feb85ee86a15ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "296624b036730fcc5804",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "62de354c75ab2dafb003",
+    "url": "/static/js/2.4e9aa9e8.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.4e9aa9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "296624b036730fcc5804",
+    "url": "/static/js/main.866b7b3b.chunk.js"
+  },
+  {
+    "revision": "872484f835a567e68f48",
+    "url": "/static/js/runtime-main.f579e466.js"
+  },
+  {
+    "revision": "d9f03bee7e8a2c379552032986e42192",
+    "url": "/static/media/travel.d9f03bee.png"
+  }
+]);
